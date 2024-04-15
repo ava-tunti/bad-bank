e4dbf3686e9ef0381e85bbbe9fc74a79dc674ac8
Ava Tuntikanokporn <150287550+ava-tunti@users.noreply.github.com>
2024-04-14 19:12:17 -0700

    Update home.js

--- home.js
+++ home.js
@@ -1,11 +1,12 @@
 function Home(){
   return (
-    <Card
+    <><Card
       txtcolor="black"
       header="BadBank Landing Module"
       title="Welcome to the bank"
       text="You can move around using the navigation bar."
-      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
-    />    
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image" />)} /><h1>
+        {JSON.stringify(ctx)}
+      </h1></>  
   );  
 }
